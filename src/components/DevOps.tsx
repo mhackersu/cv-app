eb752cd2a695ce01ed3651f22211340e5c78c0ab
Mike Hacker <mhackersu@gmail.com>
2025-02-27 22:21:47 +0000

    fix import

--- src/components/DevOps.tsx
+++ src/components/DevOps.tsx
@@ -1,10 +1,10 @@
-import Resume './Resume.mdx'
+import Resume from './Resume.mdx'
 
 function DevOps() {
   return (
-  <div className="prose prose-sm sm:prose-base lg:prose-lg xl:prose-xl 2xl:prose-2xl dark:prose-invert mx-auto">
-    <Resume />
-  </div>
+    <div className="prose prose-sm mx-auto sm:prose-base lg:prose-lg xl:prose-xl 2xl:prose-2xl dark:prose-invert">
+      <Resume />
+    </div>
   )
 }
 
